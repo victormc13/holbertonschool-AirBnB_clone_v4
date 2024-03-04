703ea7f3dd35ef65baeb0101aa28ea6055e54403
victormc13 <viktorsb14@gmail.com>
2024-03-04 12:48:43 -0500

    new: Creates a new template '2-hbnb.html' (based on 1-hbnb.html) and update it

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  let checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function() {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+  // Request to .../api/v1/status/
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data, textStatus) => {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });  
+});
